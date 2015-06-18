57aa612902e869f925620b35f17b92376561e83f
Guiligan <guiligan@gmail.com>
2015-06-18 11:26:29 -0300

    first commit

---
+++ ui.R
@@ -0,0 +1,15 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+    headerPanel('Cars fuel consumption vs aspects of design k-means clustering'),
+    sidebarPanel(
+        span("Select between different cars design aspects and the number of
+             clusters to group according to fuel effiency."),
+        selectInput('xcol', 'X Variable', names(mtcars),
+                    selected=names(mtcars)[[2]]),
+        numericInput('clusters', 'Cluster count', 3,
+                     min = 1, max = 9)
+    ),
+    mainPanel(
+        plotOutput('plot1')
+    )
+))
